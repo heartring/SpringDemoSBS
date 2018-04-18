f0d8c27bea0528e7cbb514764303a2ae89c3d984
heartring <heartring@126.com>
2018-04-18 21:41:35 +0800

    使用druid 连接池连接mysql数据库

---
+++ src/test/java/DataBaseConnectorTest.java
@@ -0,0 +1,31 @@
+import com.alibaba.druid.pool.DruidDataSource;
+import com.mysql.jdbc.Driver;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+/**
+ * Created by Administrator on 2018/4/18 0018.
+ */
+public class DataBaseConnectorTest {
+
+    /**
+     * 测试 连接池druid连接mysql
+     * @throws SQLException
+     */
+    @Test
+    public void testMysqlAndDruidConnetor() throws SQLException {
+        DruidDataSource dds = new DruidDataSource();
+        dds.setUsername("root");
+        dds.setPassword("123");
+        dds.setUrl("jdbc:mysql://localhost:3306/atguigu");
+        dds.setDriver(new Driver());
+
+        String dbName = dds.getConnection().getMetaData().getDatabaseProductName();
+        System.out.println(dbName);
+        dds.close();
+    }
+
+
+
+}
