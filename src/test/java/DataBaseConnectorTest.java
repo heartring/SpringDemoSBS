9a03468326d82dcb1d451360ff4a4d86f112e58a
heartring <heartring@126.com>
2018-04-18 22:03:33 +0800

    测试连接mongoDB

--- src/test/java/DataBaseConnectorTest.java
+++ src/test/java/DataBaseConnectorTest.java
@@ -1,4 +1,6 @@
 import com.alibaba.druid.pool.DruidDataSource;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import com.mysql.jdbc.Driver;
 import org.junit.Test;
 
@@ -24,6 +26,22 @@ public class DataBaseConnectorTest {
         String dbName = dds.getConnection().getMetaData().getDatabaseProductName();
         System.out.println(dbName);
         dds.close();
+    }
+
+
+    /**
+     * 测试连接mongoDB
+     */
+    @Test
+    public void testMongoConnection(){
+        MongoClient mc = new MongoClient("localhost", 27017);
+        MongoDatabase mdb = mc.getDatabase("test");
+        System.out.println(mdb.getName());
+        System.out.println(mdb.getCollection("stus").find());
+        mc.close();
+
+
+
     }
 
 
